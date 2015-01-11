c2b8f64cf28c08d2f47921d59d2d8ba0888fec8e
pcummuskey <pcummuskey@bravurasolutions.com>
2015-01-12 11:46:22 +1300

    Converting build to browserify.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
-var requirejs = require('gulp-requirejs');
 var browserify = require('gulp-browserify');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var stringify = require('stringify');
+var gulpif = require('gulp-if');
 
-gulp.task('run', function() {
+var mode_prod = false;
+
+gulp.task('run', [ 'build' ], function() {
 	nodemon({
 		script: 'src/server/server.js',
 		ext: 'html js css',
@@ -15,15 +19,31 @@ gulp.task('run', function() {
 	});
 });
 
-gulp.task('build', [ 'build-swarm' ], function() {
+gulp.task('build', [
+	'build-client',
+	'build-styles'
+]);
 
-	var CONFIG_REQUIREJS = {
-		baseUrl: 'src/client/require-config.js',
-		out: 'client.js'
-	};
+gulp.task('build-client', [ 'build-swarm' ], function() {
 
-	requirejs(CONFIG_REQUIREJS)
-		.pipe(gulp.dest('dist'));
+	gulp.src('src/client/*')
+		.pipe(browserify({
+			transform: stringify({
+				extensions: [ '.html' ],
+				minify: mode_prod
+			})
+		}))
+		.pipe(gulpif(mode_prod, uglify()))
+		.pipe(gulp.dest('src/server/static'));
+
+});
+
+gulp.task('build-styles', [], function() {
+
+	gulp.src('src/client/**.scss')
+		.pipe(sass())
+		.pipe(gulpif(mode_prod, uglify()))
+		.pipe(gulp.dest('src/server/static'));
 
 });
 
@@ -31,10 +51,6 @@ gulp.task('build-swarm', function() {
 
 	gulp.src('node_modules/swarm/lib/Html5Client.js')
 		.pipe(browserify())
-		.pipe(rename('swarm-client.js'))
-		.pipe(gulp.dest('src/client/lib'))
-		.pipe(uglify())
-		.pipe(rename('swarm-client.min.js'))
-		.pipe(gulp.dest('src/client/lib'));
+		.pipe(rename('swarm-client.js'));
 
 });
