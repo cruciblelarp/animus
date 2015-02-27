61168de332dc6b8232d176903e4f053543f75844
pcummuskey <pcummuskey@bravurasolutions.com>
2015-02-27 20:39:26 +1300

    Enhanced the ability editing panel. Still needs work.

--- src/client/pages/admin/abilities/detail/admin-abilities-detail-controller.js
+++ src/client/pages/admin/abilities/detail/admin-abilities-detail-controller.js
@@ -17,11 +17,48 @@ define([
 		'$scope', _swarm, '$state', '$stateParams',
 		function($scope, $swarm, $state, $stateParams) {
 
+			function setSelection(_id) {
+
+				$scope.selection = _.findWhere($scope.abilities, {
+					_id: _id
+				});
+
+				//TODO: This needs more work to actually be useful.
+				var dontIncludeList = [ $scope.selection ];
+				$scope.prerequisites = _.select($scope.abilities, function(ability) {
+
+					// Find whether there is something not to include in the list
+					var shouldNotInclude = _.find(dontIncludeList, function(notIncludedItem) {
+
+						// Is the ability already in the list for some reason?
+						if (notIncludedItem === ability) {
+							return true
+						}
+
+						// Does the ability rely on something not to include?
+						var badPrereq = _.find(ability.prerequisites, function(prerequisite) {
+							return prerequisite._id === notIncludedItem._id
+								|| prerequisite.name === notIncludedItem.name;
+						});
+
+						if (badPrereq) {
+							dontIncludeList.push(ability);
+							return true;
+						}
+
+						return false;
+
+					});
+
+					return !shouldNotInclude;
+
+				});
+
+			}
+
 			$scope.$on('$stateChangeSuccess', function() {
 				if ($state.includes('admin.abilities.detail')) {
-					$scope.selection = _.findWhere($scope.abilities, {
-						_id: parseInt($stateParams.ability)
-					});
+					setSelection(parseInt($stateParams['ability']));
 				}
 			});
 
