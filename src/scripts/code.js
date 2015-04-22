8173a53d66729e8771b70dc491320ee94de0d6c9
pcummuskey <pcummuskey@bravurasolutions.com>
2015-04-23 10:04:45 +1200

    Fixing issues with path generation.

--- src/scripts/code.js
+++ src/scripts/code.js
@@ -13,7 +13,7 @@ var config = require('./config');
 var config_requirejs = require('./requirejs');
 
 function amd_reduction(memo, item) {
-	memo.push('./' + path.relative(config.PATH_STATIC_ABS, item.path));
+	memo.push(path.relative(config.PATH_STATIC_ABS, item.path).replace('\\', '/'));
 	return memo;
 }
 
@@ -29,7 +29,7 @@ module.exports = function() {
 		.pipe(rename('debug.html'))
 		.pipe(gulp.dest(config.PATH_STATIC));
 
-	gulp.src('src/client/**.js')
+	gulp.src(config.PATH_STATIC + '/**.js')
 		.pipe(optimise('angular-bootstrap', config_requirejs))
 		.pipe(gulp.dest(config.PATH_STATIC + '/dist')) // TODO: Make compressed files sit next to uncompressed ones.
 		.pipe(reduce(amd_reduction, []))
@@ -42,7 +42,7 @@ module.exports = function() {
 				}
 			};
 
-			gulp.src('src/client/client.jade')
+			gulp.src(config.PATH_STATIC + '/client.jade')
 				.pipe(jade(config_jade))
 				.pipe(rename('client.html'))
 				.pipe(gulp.dest(config.PATH_STATIC));
