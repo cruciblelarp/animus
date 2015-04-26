e5566f220c0a9a8e0092633173464537bb761a88
pcummuskey <pcummuskey@bravurasolutions.com>
2015-04-26 22:45:34 +1200

    Changing code compilation process to bundle.

--- src/scripts/code.js
+++ src/scripts/code.js
@@ -4,50 +4,20 @@
 
 var gulp = require('gulp');
 var optimise = require('amd-optimize');
-var jade = require('gulp-jade');
-var reduce = require('stream-reduce');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 var path = require('path');
-var rename = require('gulp-rename');
 
 var config = require('./config');
 var config_requirejs = require('./requirejs');
 
-function amd_reduction(memo, item) {
-	memo.push(path.relative(config.PATH_STATIC_ABS, item.path).replace('\\', '/'));
-	return memo;
-}
-
 module.exports = function() {
-
-	var config_jade = {
-		pretty: '\t',
-		data: {}
-	};
-
-	gulp.src(config.PATH_STATIC + '/client.jade')
-		.pipe(jade(config_jade))
-		.pipe(rename('debug.html'))
-		.pipe(gulp.dest(config.PATH_STATIC));
 
 	gulp.src(config.PATH_STATIC + '/**.js')
 		.pipe(optimise('angular-bootstrap', config_requirejs))
-		.pipe(gulp.dest(config.PATH_STATIC + '/dist')) // TODO: Make compressed files sit next to uncompressed ones.
-		.pipe(reduce(amd_reduction, []))
-		.on('data', function(list) {
-
-			var config_jade = {
-				pretty: '\t',
-				data: {
-					scripts: list
-				}
-			};
-
-			gulp.src(config.PATH_STATIC + '/client.jade')
-				.pipe(jade(config_jade))
-				.pipe(rename('client.html'))
-				.pipe(gulp.dest(config.PATH_STATIC));
-
-		});
+		.pipe(concat('scripts.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest(config.PATH_STATIC));
 
 
 };
