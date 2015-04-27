bf9126a013d0c07ea75665842986dc8a10cb9034
pcummuskey <pcummuskey@bravurasolutions.com>
2015-04-27 16:07:07 +1200

    Got css minification and source maps working.

--- src/scripts/code.js
+++ src/scripts/code.js
@@ -5,6 +5,7 @@
 var gulp = require('gulp');
 var optimise = require('amd-optimize');
 var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 var path = require('path');
 
@@ -14,10 +15,11 @@ var config_requirejs = require('./requirejs');
 module.exports = function() {
 
 	gulp.src(config.PATH_STATIC + '/**.js')
+		.pipe(sourcemaps.init())
 		.pipe(optimise('angular-bootstrap', config_requirejs))
 		.pipe(concat('scripts.min.js'))
 		.pipe(uglify())
+		.pipe(sourcemaps.write())
 		.pipe(gulp.dest(config.PATH_STATIC));
-
 
 };
--- src/scripts/config.js
+++ src/scripts/config.js
@@ -9,4 +9,9 @@ $config.PATH_STATIC = 'src/server/static';
 $config.PATH_STATIC_ABS = path.resolve(__dirname + '/../..', $config.PATH_STATIC);
 $config.PATH_STATIC_LIBS = $config.PATH_STATIC + '/lib';
 
+$config.sourcemaps = {
+	sourceRoot: $config.PATH_STATIC,
+	sourceMappingURLPrefix: '.'
+};
+
 module.exports = $config;
