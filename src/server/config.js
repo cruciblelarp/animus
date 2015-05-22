76d8c339230cdd16c8ef0e068fc5dd15b7e08df0
pcummuskey <pcummuskey@bravurasolutions.com>
2015-05-23 00:27:48 +1200

    [18] Added an exit manager, and more work on mongo.

--- src/server/config.js
+++ src/server/config.js
@@ -9,6 +9,22 @@ module.exports = {
 	port: process.env['PORT'] || 8000,
 	mongo: {
 		uri: process.env['MONGOLAB_URI'] || 'mongodb://localhost:4000'
+	},
+
+	timeout: {
+		exit: 5000
+	},
+
+	constant: {
+
+		EXIT_OK: 0,
+		EXIT_MONGO: 1,
+		EXIT_HTTP: 2,
+		EXIT_SOCKET: 3,
+		EXIT_SWARM: 4,
+		EXIT_TIMEOUT: 5,
+		EXIT_ERROR: 6
+
 	}
 
 };
--- src/server/mongo.js
+++ src/server/mongo.js
@@ -1,46 +1,84 @@
 /* globals require, module */
 
 var mongodb = require('mongodb');
-var assert = require('assert');
 var Promise = require('promise');
 
 var config = require('./config');
+var exit = require('./exit');
 
-var client = null;
+var connection = null;
 
 function getConnection() {
 
-	if (client) {
-		return client;
+	if (connection) {
+		return connection;
 	}
 
-	mongodb.MongoClient.connect(config.mongo.uri, function(err, db) {
-		assert.equal(null, err);
-		client = db;
+	return connection = new Promise(function(resolve, reject) {
+
+		mongodb.MongoClient.connect(config.mongo.uri, function(error, connection) {
+			return error
+				? reject(error)
+				: resolve(connection);
+		});
+
 	});
 
 }
 
+function getCollection(name) {
+	return getConnection().then(function(db) {
+		return new Promise(function(resolve, reject) {
+			try {
+				resolve(db.collection(name));
+			} catch(error) {
+				reject(error);
+			}
+		});
+	});
+}
+
 module.exports = {
 
-	get: function(collection, query) {
-		return new Promise(function(resolve, reject) {
-			getConnection().collection(collection).find(query).toObject(function(err, item) {
-				return err
-					? reject(err)
-					: resolve(item);
+	get: function(collectionName, query) {
+		return getCollection(collectionName).then(function(collection) {
+			return new Promise(function(resolve, reject) {
+				collection.find(query).toObject(function(err, item) {
+					return err
+						? reject(err)
+						: resolve(item);
+				});
 			});
 		});
 	},
 
-	list: function(collection, query) {
-		return new Promise(function(resolve, reject) {
-			getConnection().collection(collection).find(query).toArray(function(err, list) {
-				return err
-					? reject(err)
-					: resolve(list);
+	list: function(collectionName, query) {
+		return getCollection(collectionName).then(function(collection) {
+			return new Promise(function(resolve, reject) {
+				collection.find(query).toArray(function(err, item) {
+					return err
+						? reject(err)
+						: resolve(item);
+				});
 			});
 		});
 	}
 
 };
+
+exit.listen(function(resolve) {
+
+	if (!connection) {
+		console.log('Mongo client has not been initialised.');
+		return resolve(config.constant.EXIT_OK);
+	}
+
+	return connection.then(function(conn) {
+		conn.close(true, function(error) {
+			return error
+				? resolve(config.constant.EXIT_MONGO)
+				: resolve(config.constant.EXIT_OK);
+		});
+	});
+
+});
