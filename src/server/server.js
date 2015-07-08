5320b767fd1eb48b928fa28f5243ad9e21d5e8f2
pcummuskey <pcummuskey@bravurasolutions.com>
2015-07-09 10:30:30 +1200

    [22] Adding file scanning in the rest api directory.

--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,46 @@
-/* globals require, process */
+/* globals require, process, __dirname */
+
+var fs = require('fs');
+var _ = require('underscore');
+var Promise = require('promise');
+var paths = require('path');
 
 var config = require('./config');
 var http = require('./http');
 
-require('./listeners/login-listener');
-require('./reactors/entity-reactor');
+var statFile = Promise.denodeify(fs.stat);
+var listFiles = Promise.denodeify(fs.readdir);
 
-http.listen(config.port, function() {
-	console.log('Starting application on port ' + config.port);
-}).on('error', function(error) {
-	console.error(error.stack);
+function scandir(directory) {
+	return listFiles(directory).then(function(files) {
+
+		var promises = _.collect(files, function(file) {
+			var path = paths.join(directory, file);
+			return statFile(path).then(function(stats) {
+
+				if (stats.isDirectory()) {
+					return scandir(path);
+				}
+
+				if (stats.isFile() && path.match(new RegExp('.js$'))) {
+					return Promise.resolve(require(path));
+				}
+
+				// Ignore
+				return Promise.resolve();
+
+			});
+		});
+
+		return Promise.all(_.compact(promises));
+
+	});
+}
+
+scandir(__dirname + '/api').done(function() {
+	http.listen(config.port, function() {
+		console.log('Starting application on port ' + config.port);
+	}).on('error', function(error) {
+		console.error(error.stack);
+	});
 });
