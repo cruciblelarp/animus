8d35e0da72f791f122730783cb671d7f1b1afa0e
pcummuskey <pcummuskey@bravurasolutions.com>
2015-08-10 23:37:00 +1200

    [40] Listening with Express instead of http.

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
 var config = require('./src/server/config');
 
 console.log('process.env: ' + JSON.stringify(process.env, null));
-console.log('config: ' + JSON.stringify(config  , null));
+console.log('config: ' + JSON.stringify(config, null));
 
 // Load up all the rest endpoints.
 require('./src/server/rest');
 
 // start the server.
-require('./src/server/http').listen(config.port, config.hostname, function() {
+require('./src/server/express').listen(config.port, config.hostname, function() {
 	console.log('Starting application on http://' + config.hostname + ':' + config.port + '/');
 
 }).on('error', function(error) {
